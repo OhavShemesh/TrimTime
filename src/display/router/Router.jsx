85f2869c6db4d73913e9b157bf9bd855d35bcc55
Ohav <ohav99053@gmail.com>
2025-05-12 21:38:26 +0300

    Initial commit

---
+++ src/display/router/Router.jsx
@@ -0,0 +1,10 @@
+﻿import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ROUTES, { routes } from "./routesModule";
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route path={ROUTES.HOME} element={<HomePageManager />} />
+    </Routes>
+  );
+}
